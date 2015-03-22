6b4dea18a3782f65f4c695fe4a73205adac2e7c2
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-22 14:48:34 -0400

    barebones interface now operational

--- M3 due mar 26/Q3UserInterface.java
+++ M3 due mar 26/Q3UserInterface.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 	// assuming the postgresql jar is in the same folder as this
 public class Q3UserInterface
 {
-	private static final String MENU_TEXT = "Please selected one of the following options by number only:\n"
+	private static final String MENU_TEXT = "\nPlease selected one of the following options by number:\n"
 		+ "1) Look up a thing\n"
 		+ "2) Do a fancy thing\n"
 		+ "3) Modify a thing\n"
@@ -13,8 +13,7 @@ public class Q3UserInterface
 		+ "5) Look up a cool thing\n"
 		+ "6) Quit\n";
 
-	// TODO: check if this is right... it always hangs on connection
-	private static final String USERNAME = "CS421";
+	private static final String USERNAME = "cs421g03";
 	private static final String PASSWORD = "muvfoytt";
 
     public static void main ( String [ ] args ) throws SQLException
@@ -24,7 +23,7 @@ public class Q3UserInterface
 		 	Class.forName("org.postgresql.Driver");
 		} catch(ClassNotFoundException e){e.printStackTrace();}
 	
-		String url = "jdbc:postgresql://db2:50000/CS421";
+		String url = "jdbc:postgresql://db2/CS421";
 		Connection con = DriverManager.getConnection (url, USERNAME, PASSWORD) ;
 		Statement statement = con.createStatement ( ) ;
 
@@ -36,27 +35,33 @@ public class Q3UserInterface
 		while(input != 6)
 		{
 			System.out.println(MENU_TEXT);
-			input = parseInput();
+
+			try
+			{
+				input = parseInput();
+			} catch(java.util.InputMismatchException e){ input = 2112; }
+
 
 			switch(input)
 			{
-				// TODO
-				case 1:
-					break;
-				case 2:
-					break;
-				case 4:
-					break;
-				case 5:
-					break;
-
-				default:
-					System.out.println("That is not a valid input, please try again");
-					break;
+			// TODO
+			case 1:
+				break;
+			case 2:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				System.out.println("Goodbye.");
+				break;
+			default:
+				System.out.println("That is not a valid input, please try again");
+				break;
 			}
+			System.out.println(" ");
 		}
-
-		System.out.println("Connection successful. Disconnecting.");
 		con.close ( ) ;
     }
 
